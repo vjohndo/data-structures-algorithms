8f2e02616765af3b04285c449e9ed30a6dad0e77
John Do <v.john.do@outlook.com>
2021-08-25 19:54:33 +1000

    Chapter 3 complete

--- 03_algorithm_analysis/my_own_experiments.py
+++ 03_algorithm_analysis/my_own_experiments.py
@@ -54,10 +54,14 @@ def experiment2():
 
 """
 Devise an experiement that compares the performance of the del operator on lists and dictionaries
+    > As you loop the through the timit, it deletes values and may randomly call on deleted keys.
+    > As the dictionary will be pass by reference... this is an issue.
+    > Following tests try to circumvent this.
 """
+
+# This experiment fails as dct.keys() is O(n)
 def experiment3():
     for i in range(10000,200001,20000):
-        # As you loop the through the timit, it deletes values, want to istead make it choose a random value. 
         # creating object.keys is O(n)... fail.. let's try 
         lst_test = timeit.Timer(f"del lst_object3[random.choice(range(len(lst_object3)))]",
                             'from __main__ import random, experiment3, lst_object3')
@@ -75,9 +79,7 @@ def experiment3():
 
         print(f"{i}: list-time = {lst_time} || dict-time = {dct_time}")
 
-# experiment3() -- Pass by reference, eventually random number will be called on a deleted value
-
-
+# I try to regenerate the list & dict objects but both are O(n) and I would not be able to demonstrate a constant relationship
 def experiment3A():
     for i in range(10000,200001,20000):
         lst_test = timeit.Timer(f"del list(range({i}))[random.randrange({i})]",
@@ -94,8 +96,7 @@ def experiment3A():
         
         print(f"{i}: list-time = {lst_time} || dict-time = {dct_time}")
         
-# experiment3A()
-
+# experiment3A() - Success, after each deletion, add back something to the object which is stil and O(1) event. 
 def experiment3B():
     for i in range(10000,1000001,20000):
         
@@ -119,9 +120,62 @@ def experiment3B():
         
         print(f"{i}: list-time = {lst_time} || dict-time = {dct_time}")
         
-experiment3B()
-# Given a list of number in random order write and algorithm that works in O(nlog(n)) to kind the kth smallest num
-## Sort and compare
+# experiment3B()
 
-# Can you imporve the algorithm form the proveious prblem to be linear? Explain....
-## Count and compare
+"""
+# Given a list of number in random order write and algorithm that works in O(nlog(n)) to kind the kth smallest num
+"""
+def experiment4():
+    for i in range(10000,10000001,20000):
+        # Generate a global list object and through each loop create a list
+        global lst4
+        lst4 = list(range(i))
+        random.shuffle(lst4)
+
+        lst_test = timeit.Timer("lst4.sort()\nlst4[0]","from __main__ import lst4")
+        lst_time = lst_test.timeit(number = 1000)
+
+        print(f"{i}: list-min-time = {lst_time}")
+
+# experiment4()
+    
+"""
+Can you imporve the algorithm form the proveious prblem to be linear? Explain...
+    > Count and compare, counting is a O(n)
+"""
+
+def experiment5():
+    for i in range(10000,10000001,20000):
+         # Generate a global list object and through each loop create a list
+        global lst5
+        lst5 = list(range(i))
+        random.shuffle(lst5)
+
+        lst_test = timeit.Timer("experiment5c(lst5)","from __main__ import lst5, experiment5c")
+        lst_time = lst_test.timeit(number = 1000)
+
+        lst_test_2 = timeit.Timer("experiment5b(lst5)","from __main__ import lst5, experiment5b")
+        lst_time_2 = lst_test_2.timeit(number = 1000)
+
+
+        print(f"{i}: list-min-time O(nlog(n)) = {lst_time} || list-min-time O(n) = {lst_time_2}")
+
+def experiment5a(lst):
+    min_int = lst[0]
+    for x in lst:
+        if x < min_int:
+            min_int = x
+    return min_int
+
+def experiment5b(lst):
+    return min(lst)
+
+def experiment5c(lst):
+    lst.sort()
+    return lst[0]
+
+rand_list = [-123,424,424,424020,-123,43]
+print(experiment5b(rand_list))
+print(experiment5c(rand_list))
+
+experiment5()
