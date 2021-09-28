20b65f376c0ddd3c7002d8cf3fd9c36f2ec41285
John Do <v.john.do@outlook.com>
2021-09-28 17:59:16 +1000

    sorting and searching

--- 06_sorting_and_searching/sequential_search.py
+++ 06_sorting_and_searching/sequential_search.py
@@ -9,6 +9,7 @@ def sequential_search(a_list, item):
     
     return False
 
+
 test_list = [1, 2, 32, 8, 17, 19, 42, 13, 0]
 print(sequential_search(test_list, 3))
 print(sequential_search(test_list, 13))
