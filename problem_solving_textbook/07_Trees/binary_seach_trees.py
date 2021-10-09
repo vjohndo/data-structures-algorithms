57b41a07b5d134eec3b02ced70286a1afb0a6d04
John Do <v.john.do@outlook.com>
2021-10-09 23:10:53 +1100

    binary search

--- problem_solving_textbook/07_Trees/binary_seach_trees.py
+++ problem_solving_textbook/07_Trees/binary_seach_trees.py
@@ -1,3 +1,13 @@
 class BinarySearchTree:
     def __init__(self):
-        self.hello = []
+        self.root = None
+        self.size = 0
+
+    def __len__(self):
+        return self.size
+
+    def size(self):
+        return self.size
+
+    def __iter__(self):
+        return self.root.__iter__()
