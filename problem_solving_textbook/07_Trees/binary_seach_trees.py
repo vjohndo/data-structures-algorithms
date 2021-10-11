18267fc0cfe380ecfe17ef0a43042affa07adcb7
John Do <v.john.do@outlook.com>
2021-10-11 23:01:16 +1100

    binary search tree node

--- problem_solving_textbook/07_Trees/binary_seach_trees.py
+++ problem_solving_textbook/07_Trees/binary_seach_trees.py
@@ -11,3 +11,39 @@ class BinarySearchTree:
 
     def __iter__(self):
         return self.root.__iter__()
+
+class TreeNode:
+    def __init__(self, key, value, left=None, right=None, parent=None):
+        self.key = key
+        self.value = value
+        self.left_child = left
+        self.right_child = right
+        self.parent = parent
+
+    def is_left_child(self):
+        return self.parent and self.parent.left_child is self
+
+    def is_right_child(self):
+        return self.parent and self.parent.right_child is self
+
+    def is_root(self):
+        return not self.parent
+
+    def is_leaf(self):
+        return not (self.right_child or self.left_child)
+
+    def has_a_child(self):
+        return self.right_child or self.left_child
+
+    def has_children(self):
+        return self.right_child and self.left_child
+
+    def replace_value(self, key, value, left, right):
+        self.key = key
+        self.value = value
+        self.left_child = left
+        self.right_child = right
+        if self.left_child:
+            self.left_child.parent = self
+        if self.right_child:
+            self.right_child.parent = self
