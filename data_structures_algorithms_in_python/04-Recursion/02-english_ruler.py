6ac6b28b73efe17010743d835fde879738ef5933
John Do <v.john.do@outlook.com>
2021-11-20 00:49:28 +1100

    linear recursion examples

--- data_structures_algorithms_in_python/04-Recursion/02-english_ruler.py
+++ data_structures_algorithms_in_python/04-Recursion/02-english_ruler.py
@@ -10,21 +10,21 @@ def draw_line(tick_length, tick_label = ' '):
 def draw_interval(center_length):
     """ Draw tick interval based upon a central tick length """
     if center_length > 0: # Base case would be a centre length <= 0 | O(1)
-        draw_interval(center_length - 1) # Go to a smaller interval and draw it (progression to base case) | This would result in n-1 calls, so O(n)
+        draw_interval(center_length - 1) # Go to a smaller interval and draw it (progression to base case) 
         draw_line(center_length) # Draw the centre
-        draw_interval(center_length - 1) # Go to a smaller interval and draw it (progression to base case) | This would result in n-1 calls, so O(n)
+        draw_interval(center_length - 1) # Go to a smaller interval and draw it (progression to base case) 
 
     # else the base case is do nothing, reached the smallest interval
 
 def draw_ruler(num_inches, major_length):
     """ Draw English rule with give number of inches, major tick length. """
 
-    # O(n^2)
+    # O(n^2)... look at the draw_interval
 
     # Managed the construction of the entire ruler
     draw_line(major_length, '0') # Deaw the starting line | O(1)
     for j in range(1, 1 + num_inches): # Go through each of intervals | O(n)
-        draw_interval(major_length - 1) # Draw the small interval | O(n) times, each activation is O(n)
+        draw_interval(major_length - 1) # Draw the small interval | O(2^n) times --> 
         draw_line(major_length, str(j)) # Draw the end of the that "inch" | O(1)
 
 draw_ruler(10, 2)
