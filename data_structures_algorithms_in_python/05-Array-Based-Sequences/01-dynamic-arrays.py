0632867b1c68d225fca36a73a0cf4764a53bbd12
John Do <v.john.do@outlook.com>
2021-11-21 23:14:19 +1100

    array-based sequences

--- data_structures_algorithms_in_python/05-Array-Based-Sequences/01-dynamic-arrays.py
+++ data_structures_algorithms_in_python/05-Array-Based-Sequences/01-dynamic-arrays.py
@@ -1,6 +1,6 @@
 import sys
 data = []
-for k in range(n):
+for k in range(n): # Put in a value of n that is fixed
     a = len(data)
     b = sys.getsizeof(data)
     print('Length: {0:3d}; Size in bytes: {1:4d}'.format(a,b))
