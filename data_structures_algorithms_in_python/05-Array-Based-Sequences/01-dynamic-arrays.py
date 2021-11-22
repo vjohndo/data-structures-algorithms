b92f959b2ba847282501cb14162eb7f77b1b091b
John Do <v.john.do@outlook.com>
2021-11-22 23:03:26 +1100

    array implementation

--- data_structures_algorithms_in_python/05-Array-Based-Sequences/01-dynamic-arrays.py
+++ data_structures_algorithms_in_python/05-Array-Based-Sequences/01-dynamic-arrays.py
@@ -1,7 +1,11 @@
 import sys
+
+# Initialise an empty array
 data = []
-for k in range(n): # Put in a value of n that is fixed
-    a = len(data)
-    b = sys.getsizeof(data)
+
+# Go through 26 iterations of 
+for k in range(27): # Put in a value of n that is fixed
+    a = len(data) # Get the length of references in the referential list
+    b = sys.getsizeof(data) # Get the size of the data
     print('Length: {0:3d}; Size in bytes: {1:4d}'.format(a,b))
     data.append(None)
