fa6e8bc34474f98dc10784232eb6e2398ec8c858
John Do <v.john.do@outlook.com>
2021-12-02 00:09:29 +1100

    palindrome implementation without string conversion

--- practice_questions/lc/0009-palindrome.py
+++ practice_questions/lc/0009-palindrome.py
@@ -20,4 +20,29 @@ class Solution(object):
         if x < 0:
             return False
 
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        # Want to pass through 0 as an palindrome, anything ending in 0 can never be palindrom
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+        
+        # Start a reverse palindrom at 0
+        y = 0
+        
+        # We're interested in running the loop until the reverse 'y' is > x
+        while x >= y:
+            y = (y * 10) + (x % 10)
+            # Check after adding reversed number (even palindrom)
+            if x == y:
+                return True
+            x = (x // 10)
+            # Check after removing middle palindrome (odd integered)
+            if x == y:
+                return True
+    
+        return False
         
