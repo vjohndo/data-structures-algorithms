375611b5168efa5593e524de81807db31f496d16
John Do <v.john.do@outlook.com>
2021-12-06 23:07:43 +1100

    practice

--- practice_questions/lc/0014-longest-common-prefix.py
+++ practice_questions/lc/0014-longest-common-prefix.py
@@ -1,1 +1,19 @@
-test
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        
+        common_prefix = list(strs[0])
+        
+        for string in strs[1:]:
+            
+            if len(common_prefix) > len(string):
+                common_prefix = common_prefix[0:len(string)]
+            
+            for (index, char) in enumerate(common_prefix):
+                if char != string[index]:
+                    common_prefix = common_prefix[0:index]
+        
+        return "".join(common_prefix)
