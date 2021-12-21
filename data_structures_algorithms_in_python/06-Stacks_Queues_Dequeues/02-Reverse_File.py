f197f7355e5378969223cb7ad23bca920a6f8cac
John Do <v.john.do@outlook.com>
2021-12-21 23:49:18 +1100

    stacks

--- data_structures_algorithms_in_python/06-Stacks_Queues_Dequeues/02-Reverse_File.py
+++ data_structures_algorithms_in_python/06-Stacks_Queues_Dequeues/02-Reverse_File.py
@@ -14,3 +14,18 @@ def reverse_file(filename):
         output.write(S.pop() + '\n')
 
     output.close()
+
+def is_matched(expr):
+    """Return True if all delimiters are properly matchedd"""
+    lefty = '({['
+    righty = ')}]'
+    S = ArrayStack()
+    for c in expr:
+        if c in lefty:
+            S.push(c)
+        elif c in righty:
+            if S.is_empty():
+                return False
+            if righty.index(c) != lefty.index(S.pop()):
+                return False
+    return S.is_empty()
