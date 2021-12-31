f36e73f23ec51d9e1bec36787b2e911035e01044
John <v.john.do@outlook.com>
2021-12-31 13:50:07 +1100

    optimised longest substring --> sliding window

--- practice_questions/lc/0003-longest-substring.py
+++ practice_questions/lc/0003-longest-substring.py
@@ -1,3 +1,5 @@
+# Sliding Window Patten
+
 def lengthOfLongestSubstring(s):
     """
     :type s: str
@@ -40,4 +42,72 @@ def lengthOfLongestSubstring(s):
     return max_length
 
 
-print(lengthOfLongestSubstring('dvdf'))
+def lengthOfLongestSubstring2(s):
+
+        # Runtime: 60 ms, faster than 49.84% of Python online submissions for Longest Substring Without Repeating Characters.
+        # Memory Usage: 13.5 MB, less than 97.20% of Python online submissions for Longest Substring Without Repeating Characters.
+        
+        if len(s) <= 0:
+            return 0
+        
+        if len(s) == 1:
+            return 1
+        
+        
+        start_index = 0
+        end_index = 1
+        seen_chars = {s[0]: 0}
+        max_length = 0
+        
+        
+        while end_index < len(s):
+            # Check if that you've seen this before
+            # If you have move the next index after, there may be inbetween chars not yet accounted for
+            # Let's check if that index is less than our starting index
+
+            if (s[end_index] in seen_chars) and (seen_chars[s[end_index]] >= start_index):
+                start_index = seen_chars[s[end_index]] + 1
+                print(s[start_index:end_index + 1])
+            
+            seen_chars[s[end_index]] = end_index
+
+            end_index += 1
+            
+            if end_index - start_index > max_length:
+                
+                max_length = end_index - start_index
+
+            
+        
+        return max_length
+
+
+# Runtime: 52 ms, faster than 63.94% of Python online submissions for Longest Substring Without Repeating Characters. 
+# Memory Usage: 13.7 MB, less than 58.88% of Python online submissions for Longest Substring Without Repeating Characters.
+
+def lengthOfLongestSubstring3(s):
+    if len(s) <= 0:
+            return 0
+        
+    if len(s) == 1:
+        return 1
+
+    last_index = {}
+    max_length = 0
+    start_index = 0
+
+    for current_index, current_char in enumerate(s):
+
+        # If we've seen this character before, update the starting index
+        if current_char in last_index:
+            start_index = max(start_index, last_index[current_char] + 1)
+
+        # Update to the latest last index
+        last_index[current_char] = current_index
+
+        # update the max length if there's space
+        max_length = max(max_length, current_index - start_index + 1)
+
+    return max_length
+
+print(lengthOfLongestSubstring3('dvdf'))
