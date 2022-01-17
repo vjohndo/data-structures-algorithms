402633405bbc056cf23ed504f896eb81d2295aa3
John <v.john.do@outlook.com>
2022-01-17 22:02:41 +1100

    dynamic programming, longest common subsequence

--- data_structures_algorithms_in_python/13-Text_Processing/DynamicProgramming.py
+++ data_structures_algorithms_in_python/13-Text_Processing/DynamicProgramming.py
@@ -11,4 +11,35 @@ def matrix_chain(d):
             j = i + b # end of the subchain
             N[i][j] = min(N[i][k] + N[k+1][j] + d[i]*d[k+1]*d[j+1] for k in range(i, j))
     return N
-    
+
+# -- Longest Common Sequence
+
+def LCS(X, Y):
+    """Return table such that L[j][k] is length of LCS for X[0:j] and Y[0:k]"""
+    n, m = len(X), len(Y) # introduct convenient notations
+    L = [[0] * (m+1) for k in range(n+1)]
+    for j in range(n):
+        for k in range(m):
+            if X[j] == Y[k]:
+                L[j+1][k+1] = L[j][k] + 1
+            else:
+                L[j+1][k+1] = max([j][k+1], L[j+1][k])
+    return L
+
+def LCS_solution(X, Y, L):
+    """Return the longest common substring of X and Y, given LCS table L."""
+    solution = []
+    j, k = len(X), len(Y)
+    while L[j][k] > 0: # Keep looping until we hit a base condition
+        if X[j-1] == Y[k-1]: # If X == Y --> case 1, must be 
+            solution.append(X[j-1]) 
+            j -= 1
+            k -= 1
+
+        # If no match, move to the larger of the the previous one
+        elif L[j-1][k] >= L[j][k-1]:
+            j -= 1
+        else:
+            k -= 1
+
+        return ''.join(reversed(solution))
