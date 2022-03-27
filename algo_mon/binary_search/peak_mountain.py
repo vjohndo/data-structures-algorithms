48022ca95d12dd8f44cf2cdd44c1a6194ff87d0a
vjohndo <v.john.do@outlook.com>
2022-03-27 19:02:01 +1100

    more binary search

---
+++ algo_mon/binary_search/peak_mountain.py
@@ -0,0 +1,20 @@
+from typing import List
+
+def peak_of_mountain_array(arr: List[int]) -> int:
+    left, right = 0, len(arr) - 1
+    boundary_index = -1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] > arr[mid + 1]:
+            boundary_index = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return boundary_index
+
+if __name__ == '__main__':
+    arr = [int(x) for x in input().split()]
+    res = peak_of_mountain_array(arr)
+    print(res)
