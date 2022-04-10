3fb227eb19c21d8e316732548d734808d197d421
vjohndo <v.john.do@outlook.com>
2022-04-10 22:54:47 +1000

    dfs

---
+++ algo_mon/ch03_depth_first_search/ex05_visible_tree_node.py
@@ -0,0 +1,21 @@
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def visible_tree_node(root: Node) -> int:
+    # WRITE YOUR BRILLIANT CODE HERE
+    return 0
+
+def build_tree(nodes, f):
+    val = next(nodes)
+    if val == 'x': return None
+    left = build_tree(nodes, f)
+    right = build_tree(nodes, f)
+    return Node(f(val), left, right)
+
+if __name__ == '__main__':
+    root = build_tree(iter(input().split()), int)
+    res = visible_tree_node(root)
+    print(res)
